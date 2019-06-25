f08de239111da26d2b7db7a276d5ce51c891b8ef
Clarence <clarence@github.com>
2019-06-26 00:05:00 +0100

    Update

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const tabIcons = document.querySelectorAll('.tab');
+const tabContent = document.querySelectorAll('.project');
+const scrollBtn = document.querySelectorAll('.scroll-p');
+let timeOut = "";
+
+function selectTab(e){
+	removeBorder();
+	removeShow();
+	this.classList.add('border');
+	const shots = document.querySelector(`#${this.id}-content`);
+	shots.classList.add('show');
+	
+}
+
+
+function scrollToTop(e){
+	//let position = document.body.scrollToTop || document.documentElement.scrollToTop;
+	if(document.body.scrollTop !=0 || document.documentElement.scrollTop != 0){
+		//window.scrollBy(0, -Math.max(10, Math.floor(position / 10)));
+		//scrollAnimation=setTimeout('scrollToTop()',10);
+
+		window.scrollBy(0, -50);
+		//timeOut = setTimeout('scrollToTop()', 10);
+
+		requestAnimationFrame(scrollToTop);
+		hide();
+
+	}else{
+		clearTimeout(timeOut);
+	}
+}
+
+function hide(){
+	scrollBtn.forEach(scroll => {
+		setTimeout(scroll.classList.add('hide'), 5000);
+	});
+}
+
+
+
+
+
+function removeBorder(){
+	tabIcons.forEach(icon => icon.classList.remove('border'));
+}
+
+function removeShow(){
+	tabContent.forEach(icon => icon.classList.remove('show'));
+}
+
+tabIcons.forEach(icon => icon.addEventListener('click', selectTab));
+
+scrollBtn.forEach(scroll => scroll.addEventListener('click', scrollToTop))
